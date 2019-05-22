7e5a683a99e32cd6d8fe6defaa2b56f75771a4d7
mfadhil07 <crewtakengon07@gmail.com>
2019-05-22 11:45:54 +0700

    Form Login down

--- JustCheckList/application/config/autoload.php
+++ JustCheckList/application/config/autoload.php
@@ -20,7 +20,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('email', 'session');
+$autoload['libraries'] = array('email', 'session', 'database');
 
 /*
 | -------------------------------------------------------------------
@@ -51,7 +51,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'security', 'file');
 
 /*
 | -------------------------------------------------------------------
