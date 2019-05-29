9522d28213d4458f205bb8b20b4380474d00eb7b
mfadhil07 <crewtakengon07@gmail.com>
2019-05-29 17:59:27 +0700

    berangkat dan priview down

---
+++ JustCheckList/application/models/tb_member_model.php
@@ -0,0 +1,11 @@
+<?php
+class tb_member_model extends CI_Model
+{
+
+    public function getAnggotaByIdBos($id)
+    {
+        $this->db->where('id_member', $id);
+
+        return $this->db->get('tb_member')->result();
+    }
+}
